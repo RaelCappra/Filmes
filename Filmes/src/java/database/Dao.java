0a76dad0ed9661e56458c06d2a1618c1ee30ecd4
Rael Cappra <rael.cappra@gmail.com>
2015-06-26 09:30:52 -0300

    adicionada a interface Dao

---
+++ Filmes/src/java/database/Dao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import java.util.List;
+
+/**
+ *
+ * @author Aluno
+ * @param <T>
+ * @param <PK>
+ */
+public interface Dao<T, PK> {
+
+    void save(T entity);
+
+    void delete(PK id);
+
+    List<T> listAll();
+    
+    T getById(PK pk);
+
+}
