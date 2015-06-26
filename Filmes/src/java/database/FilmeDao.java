423dff38145887d7af33480b87ba9eb03a1cd66f
Rael Cappra <rael.cappra@gmail.com>
2015-06-26 10:04:07 -0300

    Implementado save() em FilmeDao

--- Filmes/src/java/database/FilmeDao.java
+++ Filmes/src/java/database/FilmeDao.java
@@ -5,6 +5,9 @@
  */
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,7 +32,23 @@ public class FilmeDao implements Dao<Filme, Long> {
 
     @Override
     public void save(Filme entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "insert into filme (titulo, genero, classificacao,"
+                + " direcao, elenco, sinopse, link_trailer, duracao) "
+                + "values (?,?,?,?,?,?,?,?)";
+        Connection connection = conexao.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, entity.getTitulo());
+            ps.setLong(2, entity.getGenero().getId());
+            ps.setLong(3, entity.getClassificacao().getId());
+            ps.setString(4, entity.getDirecao());
+            ps.setString(5, entity.getSinopse());
+            ps.setString(6, entity.getLinkTrailer());
+            ps.setString(7, entity.getDuracaoMinutos() + " minutes"); //TODO:Check this
+            ps.executeQuery();
+        } catch (SQLException ex) {
+            //TODO: tratar
+            Logger.getLogger(GeneroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
